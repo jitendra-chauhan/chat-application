982fa231b6576b3a7540f36f722cab7f59458d22
jitendra chauhan <jitendrachauhan527@gmail.com>
2023-12-27 18:10:37 +0530

    revort code after test

--- src/connections/mongo.ts
+++ src/connections/mongo.ts
@@ -1,5 +1,4 @@
-// const { MongoClient } = require("mongodb");
-import { MongoClient,ObjectId } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import { getConfig } from "./config";
 
 class MongoDB {
@@ -14,54 +13,29 @@ class MongoDB {
   }
 
   getUrl(DB_PROTO: string, DB_HOST: string, DB_PORT: string, DB_NAME: string) {
-    return `${DB_PROTO}://${DB_HOST}:${DB_PORT}/${DB_NAME}`; //?retryWrites=true&w=majority
+    return `${DB_PROTO}://${DB_HOST}:${DB_PORT}/${DB_NAME}`; 
   }
 
   // connection setup
   async connection(resolve: any, reject: any) {
     try {
-      const {main} = await import("../main");
-    
-      console.log("connetcion :::: 11");
+      const { main } = await import("../main");
       const client = await new MongoClient(
-        // this.url, 
-        'mongodb://24xbet:TriO9944@139.59.46.155:27017/24xbetdb',
-        );
-        console.log("connetcion :::: 22");
-    
-    
-    await client.connect();
-    this.db = client.db(this.DB_NAME);
-    main.DB.init(this.db, client);
-    
-    console.log(" connected ");
-    
-    resolve(this.db);
+        this.url,
+      );
+      await client.connect();
+      this.db = client.db(this.DB_NAME);
+      main.DB.init(this.db, client);
+
+      console.log(" connected ");
+
+      resolve(this.db);
     } catch (error) {
-        console.log(" error : " , error);
-      
+      console.log(" error : ", error);
+
       reject(error)
     }
 
-    // const {main} = await import("../main");
-    // MongoClient.connect(
-    //   this.url,
-    //   { useUnifiedTopology: true, useNewUrlParser: true },
-    //   (err: any, client: any) => {
-    //     console.log("===> error <====", err);
-
-    //     if (err) reject(err);
-
-    //     this.db = client.db(this.DB_NAME);
-
-    //     main.DB.init(this.db, client);
-
-    //     console.info("DB Connected successfully!");
-
-    //     resolve(this.db);
-    //   }
-    // );
-    
   }
   async init() {
     const { DB_HOST, DB_PORT, DB_NAME }: any = getConfig();
